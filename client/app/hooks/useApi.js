7c30b6924bf91d7f1f107db9e106543841dffa78
Fabian Pasin <fabian.pasin@gmail.com>
2021-01-02 19:02:02 +0100

    Add Caching/LocalStorage

--- client/app/hooks/useApi.js
+++ client/app/hooks/useApi.js
@@ -1,25 +1,38 @@
 import { useState, useEffect } from "react";
+import cache from "../utilities/cache";
 
 const baseURL = "http://localhost:3002/";
 
-export default function useFetch(url) {
-  const [data, setData] = useState(null);
+ const useFetch = async (url) => {
+  const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(baseURL + url)
      .then(res => {
-       if(res.ok) return res.json();
+       if(res.ok) {
+         const response = res.json()
+         return response;
+       }
        setError(res);
      })
-     .then(data => setData(data))
+     .then(data => {
+       cache.storeData(url, data);
+       setData(data);
+     })
      .catch(err => {
+       const data = cache.getData(url);
+       data.ok ? setData(data.data) : null
        console.log(err);
        setError(err);
      })
-     .finally(() => setLoading(false))
+      .finally(() => setLoading(false))
   }, [])
-
+  
   return { data, loading, error }
 };
+
+export default {
+  useFetch
+}
